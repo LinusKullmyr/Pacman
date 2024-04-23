5dcd85a9d5b1d81839699f430eab60f5a7eeac5f
kambei86 <157814893+kambei86@users.noreply.github.com>
2024-04-23 15:21:58 +0200

    Remove lightning and cleaned up

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -18,17 +18,20 @@ from learningAgents import ReinforcementAgent
 import game
 import qnets
 import random
-import util
+
+# import util
 
 # import math
 import torch
 import collections
 import time
-import pytorch_lightning as pl
-import logging
+
+# import pytorch_lightning as pl
+# import logging
+import warnings
 
 
-class QLearningAgent(ReinforcementAgent):
+class DQAgent(ReinforcementAgent):
     """
     Q-Learning Agent
 
@@ -52,33 +55,40 @@ class QLearningAgent(ReinforcementAgent):
     def __init__(self, **args):
         ReinforcementAgent.__init__(self, **args)
 
-        self.alpha = 0.0001  # learning_rate
-        self.gamma = 1.0  # discount rate
+        self.index = 0  # This is always Pacman
+
+        self.learning_rate = 0.001  # learning_rate
+        self.gamma = 0.95  # discount rate
         self.epsilon_min = 0.1  # minimum random rate
-        self.epsilon_max = 0.5
+        self.epsilon_max = 0.5  # maximum random rate (initial)
         self.epsilon_scale = self.numTraining * 0.75
-        self.replay_buffer_size = 10000
+        self.replay_buffer_size = 100
         self.batch_size = 32
 
-        self.sync_target_training_count = 1000
-        self.num_training_loops = 0
-        self.sync_next_gameover = False
-
-        logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)  # Set the logging level
-        self.trainer = pl.Trainer(max_epochs=1, enable_progress_bar=False)
+        self.sync_target_episode_count = 100
 
         self.double_Q = None  # defer the creation of Q-nets until we have a state, in registerInitialState
         self.replay_buffer = collections.deque(maxlen=self.replay_buffer_size)
+
         self.action_tuples = (
-            game.Directions.STOP,
             game.Directions.NORTH,
             game.Directions.EAST,
             game.Directions.SOUTH,
             game.Directions.WEST,
+            game.Directions.STOP,
         )
 
-        self.direction_to_index_map = {"Stop": 0, "North": 1, "East": 2, "South": 3, "West": 4}
-        self.index_to_direction_map = {0: "Stop", 1: "North", 2: "East", 3: "South", 4: "West"}
+        self.direction_to_index_map = {
+            "North": 0,
+            "East": 1,
+            "South": 2,
+            "West": 3,
+            "Stop": 4,
+        }
+        self.index_to_direction_map = {0: "North", 1: "East", 2: "South", 3: "West", 4: "Stop"}
+
+        # Ignore specific UserWarnings from PyTorch about Lazy modules
+        warnings.filterwarnings("ignore", message="Lazy modules are a new feature")
 
         # From parent class init
         # self.episodesSoFar = 0
@@ -95,8 +105,8 @@ class QLearningAgent(ReinforcementAgent):
     def registerInitialState(self, state):
         # This will be called at the beginning of each episode
         self.startEpisode()
-        # if self.episodesSoFar == 0:
-        #     print("Beginning %d episodes of Training" % (self.numTraining))
+        if self.episodesSoFar == 0:
+            print("Beginning %d episodes of Training" % (self.numTraining))
 
         # Create Q-nets (once)
         if self.double_Q is None:
@@ -104,41 +114,7 @@ class QLearningAgent(ReinforcementAgent):
             num_ghosts = len(state.getGhostStates())
             input_channels = 4 + num_ghosts * 5
             output_size = 5
-            self.double_Q = qnets.DQN(input_channels, output_size, self.gamma, self.alpha)
-
-    # def getQValue(self, state, action):
-    #     """
-    #     Returns Q(state,action)
-    #     Should return 0.0 if we have never seen a state
-    #     or the Q node value otherwise
-    #     """
-    #     "*** YOUR CODE HERE ***"
-
-    #     print("getQvalue")
-    #     # util.raiseNotDefined()
-
-    # def computeValueFromQValues(self, state):
-    #     """
-    #     Returns max_action Q(state,action)
-    #     where the max is over legal actions.  Note that if
-    #     there are no legal actions, which is the case at the
-    #     terminal state, you should return a value of 0.0.
-    #     """
-    #     "*** YOUR CODE HERE ***"
-
-    #     print("computeValueFromQValues")
-    #     # util.raiseNotDefined()
-
-    # def computeActionFromQValues(self, state):
-    #     """
-    #     Compute the best action to take in a state.  Note that if there
-    #     are no legal actions, which is the case at the terminal state,
-    #     you should return None.
-    #     """
-    #     "*** YOUR CODE HERE ***"
-
-    #     print("computeActionFromQValues")
-    #     # util.raiseNotDefined()
+            self.double_Q = qnets.double_DQN(input_channels, output_size, self.learning_rate, self.gamma)
 
     def stateToTensor(self, state):
         """
@@ -150,7 +126,7 @@ class QLearningAgent(ReinforcementAgent):
 
         Returns
         -------
-        state_tensor : torch tensor of dim [8+num_ghosts*5, H, W]
+        state_tensor : torch tensor of dim [4 + num_ghosts * 5, H, W]
 
         """
         # Extracting the basic game state components
@@ -243,23 +219,26 @@ class QLearningAgent(ReinforcementAgent):
         else:
             # Get action from Q-network
             state_tensor = self.stateToTensor(state)
-            self.double_Q.eval()
             with torch.no_grad():
-                q_values = self.double_Q(state_tensor).squeeze(0)
+                q_values = self.double_Q.predict_eval(state_tensor).squeeze(0).detach()
 
-            assert q_values.shape == (5,)
+            # assert q_values.shape == (4,)
             illegal_actions_mask = torch.ones_like(q_values).bool()
             illegal_actions_mask[legal_action_ints] = False
             q_values[illegal_actions_mask] = float("-inf")
 
             action_idx = torch.argmax(q_values)
 
-        return self.action_tuples[action_idx]
+        action = self.action_tuples[action_idx]
+
+        self.doAction(state, action)
+
+        return action
 
     def reinforce(self, batch):
         states, actions, next_states, rewards, dones = zip(*batch)
 
-        # Create tensors for the batch
+        # Create stack/cat tensor for the batch
         states = torch.cat(states, dim=0)
         actions = torch.stack(actions)
         rewards = torch.stack(rewards)
@@ -272,15 +251,9 @@ class QLearningAgent(ReinforcementAgent):
         # print("next_states.shape", next_states.shape)
         # print("dones.shape", dones.shape)
 
-        # Create dataset and DataLoader
-        dataset = torch.utils.data.TensorDataset(states, actions, rewards, next_states, dones)
-        loader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+        batch = (states, actions, next_states, rewards, dones)
 
-        # Run training
-        self.trainer.fit(self.double_Q, loader)
-        self.num_training_loops += 1
-        if self.num_training_loops % self.sync_target_training_count == 0:
-            self.sync_next_gameover = True
+        self.double_Q.training_step(batch)
 
     def update(self, state, action, nextState, reward):
         """
@@ -312,13 +285,14 @@ class QLearningAgent(ReinforcementAgent):
         # Run reinforcement if replay buffer is full
         if len(self.replay_buffer) >= self.replay_buffer_size:
             batch = random.sample(list(self.replay_buffer), self.batch_size)
+
             self.reinforce(batch)
 
-    def getPolicy(self, state):
-        return self.computeActionFromQValues(state)
+    # def getPolicy(self, state):
+    #     return self.computeActionFromQValues(state)
 
-    def getValue(self, state):
-        return self.computeValueFromQValues(state)
+    # def getValue(self, state):
+    #     return self.computeValueFromQValues(state)
 
     def final(self, state):
         """
@@ -358,83 +332,38 @@ class QLearningAgent(ReinforcementAgent):
 
         ## above is from parent class!
 
-        if self.sync_next_gameover:
+        if len(self.replay_buffer) == self.replay_buffer_size and self.episodesSoFar % self.sync_target_episode_count == 0:
             print(f"Episode {self.episodesSoFar}: Synchronizing policy and target networks")
             self.syncNetworks()
-            self.sync_next_gameover = False
 
 
-class PacmanQAgent(QLearningAgent):
-    "Exactly the same as QLearningAgent, but with different default parameters"
+# class PacmanQAgent(QLearningAgent):
+#     "Exactly the same as QLearningAgent, but with different default parameters"
 
-    def __init__(self, epsilon=0.001, gamma=0.9, alpha=0.1, numTraining=10, **args):
-        """
-        These default parameters can be changed from the pacman.py command line.
-        For example, to change the exploration rate, try:
-            python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
+#     def __init__(self, epsilon=0.001, gamma=0.9, alpha=0.1, numTraining=10, **args):
+#         """
+#         These default parameters can be changed from the pacman.py command line.
+#         For example, to change the exploration rate, try:
+#             python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
 
-        alpha    - learning rate
-        epsilon  - exploration rate
-        gamma    - discount factor
-        numTraining - number of training episodes, i.e. no learning after these many episodes
-        """
-        args["epsilon"] = epsilon
-        args["gamma"] = gamma
-        args["alpha"] = alpha
-        args["numTraining"] = numTraining
-        self.index = 0  # This is always Pacman
-        QLearningAgent.__init__(self, **args)
+#         alpha    - learning rate
+#         epsilon  - exploration rate
+#         gamma    - discount factor
+#         numTraining - number of training episodes, i.e. no learning after these many episodes
+#         """
+#         args["epsilon"] = epsilon
+#         args["gamma"] = gamma
+#         args["alpha"] = alpha
+#         args["numTraining"] = numTraining
+#         self.index = 0  # This is always Pacman
+#         QLearningAgent.__init__(self, **args)
 
-    def getAction(self, state):
-        """
-        Simply calls the getAction method of QLearningAgent and then
-        informs parent of action for Pacman.  Do not change or remove this
-        method.
-        """
-        action = QLearningAgent.getAction(self, state)
-        self.doAction(state, action)
-        return action
-
-
-class ApproximateQAgent(PacmanQAgent):
-    """
-    ApproximateQLearningAgent
-
-    You should only have to overwrite getQValue
-    and update.  All other QLearningAgent functions
-    should work as is.
-    """
-
-    def __init__(self, extractor="IdentityExtractor", **args):
-        self.featExtractor = util.lookup(extractor, globals())()
-        PacmanQAgent.__init__(self, **args)
-        self.weights = util.Counter()
-
-    def getWeights(self):
-        return self.weights
-
-    def getQValue(self, state, action):
-        """
-        Should return Q(state,action) = w * featureVector
-        where * is the dotProduct operator
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-    def update(self, state, action, nextState, reward):
-        """
-        Should update your weights based on transition
-        """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-    def final(self, state):
-        "Called at the end of each game."
-        # call the super-class final method
-        PacmanQAgent.final(self, state)
-
-        # did we finish training?
-        if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
-            "*** YOUR CODE HERE ***"
-            pass
+#     def getAction(self, state):
+#         """
+#         Simply calls the getAction method of QLearningAgent and then
+#         informs parent of action for Pacman.  Do not change or remove this
+#         method.
+#         """
+#         action = QLearningAgent.getAction(self, state)
+#         self.doAction(state, action)
+#         return action
--- qnets.py
+++ qnets.py
@@ -1,60 +1,93 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import pytorch_lightning as pl
 
 
-class DQN(pl.LightningModule):
-    def __init__(self, input_channels, output_size, gamma, learning_rate):
-        super().__init__()
-        self.save_hyperparameters()
+class double_DQN:
+    def __init__(self, input_channels, output_size, learning_rate, gamma):
+        self.device = self.get_device()
+        print("Using device: ", self.device)
+
+        self.learning_rate = learning_rate
         self.gamma = gamma
 
-        # Initialize the policy and target networks
-        self.policy_network = self.create_network(input_channels, output_size)
-        self.target_network = self.create_network(input_channels, output_size)
-        self.update_target_network()
-        self.target_network.eval()  # Set target network to evaluation mode
+        self.policy_network = CNN(input_channels, output_size).to(self.device)
+        self.target_network = CNN(input_channels, output_size).to(self.device)
+        self.target_network.eval()  # Only evals from target network
+        self.update_target_network()  # Do an inital sync
 
-    def create_network(self, input_channels, output_size):
-        return nn.Sequential(
+        self.optimizer = torch.optim.Adam(self.policy_network.parameters(), lr=self.learning_rate)
+
+    def get_device(self):
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif torch.backends.mps.is_available():
+            device = "mps"
+        else:
+            device = "cpu"
+
+        return device
+
+    def update_target_network(self):
+        self.target_network.load_state_dict(self.policy_network.state_dict())
+
+    def predict_eval(self, state_tensor):
+        state_tensor = state_tensor.to(self.device)
+        self.policy_network.eval()
+        with torch.no_grad():
+            action = self.policy_network(state_tensor).detach().cpu()
+
+        return action
+
+    def training_step(self, batch):
+        states, actions, next_states, rewards, dones = (t.to(self.device) for t in batch)
+
+        # Put policy network in training mode
+        self.policy_network.train()
+
+        # Predict current state Q-values from policy network
+        current_q_values = self.policy_network(states).gather(1, actions).squeeze(-1)
+
+        # Predict next state Q-values from target network
+        with torch.no_grad():
+            next_q_values = self.target_network(next_states).max(1)[0].detach()
+
+        # mask out done states
+        next_q_values[dones.squeeze(-1)] = 0.0
+
+        # Calculate expected Q values
+        expected_q_values = rewards.squeeze(-1) + self.gamma * next_q_values
+
+        assert current_q_values.shape == expected_q_values.shape
+
+        # Compute loss
+        loss = nn.functional.mse_loss(current_q_values, expected_q_values)
+
+        # backward prop and optimization
+        self.optimizer.zero_grad(set_to_none=True)  # In-place zeroing of gradients
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.detach().cpu()
+
+
+class CNN(nn.Module):
+    def __init__(self, input_channels, output_size):
+        super().__init__()
+
+        self.model = nn.Sequential(
             nn.Conv2d(input_channels, 16, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(2),
             nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.Flatten(),
             nn.LazyLinear(128),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Linear(128, output_size),
         )
 
     def forward(self, x):
-        return self.policy_network(x)
-
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
-        return optimizer
-
-    def training_step(self, batch, batch_idx=0):
-        states, actions, rewards, next_states, dones = batch
-
-        # Predict current state Q-values
-        current_q_values = self.policy_network(states).gather(1, actions)
-
-        # Predict next state Q-values
-        next_q_values = self.target_network(next_states).max(1)[0].detach()  # No gradient tracking
-
-        # Calculate expected Q values
-        expected_q_values = rewards.squeeze(-1) + self.gamma * next_q_values * ~(dones.squeeze(-1))
-
-        # Compute loss
-        loss = nn.functional.mse_loss(current_q_values.squeeze(-1), expected_q_values)
-
-        # Logging and optimization
-        self.log("train_loss", loss)
-        return loss
-
-    def update_target_network(self):
-        self.target_network.load_state_dict(self.policy_network.state_dict())
+        return self.model(x)
