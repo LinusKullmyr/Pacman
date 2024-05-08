9b5c2cd967c69a01044dac6a2965343e32b42e25
Felix Waldschock <felix.waldschock@hotmail.com>
2024-05-08 17:05:36 +0200

    Changed the best average, to be compute every iteration. Not only if %100 == 0

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -98,6 +98,9 @@ class DQAgent(ReinforcementAgent):
         # self.alpha = float(alpha)
         # self.discount = float(gamma)
 
+        self.movingAverageWindowSize = 100
+        self.Reward_Tracker_100 = []
+
         # Feature for the logger
         self.Reward_Tracker = []
         self.GameLength = 0
@@ -363,7 +366,6 @@ class DQAgent(ReinforcementAgent):
         self.observeTransition(self.lastState, self.lastAction, state, deltaReward)
         self.stopEpisode()
 
-
         # Make sure we have this var
         if not "episodeStartTime" in self.__dict__:
             self.episodeStartTime = time.time()
@@ -371,18 +373,23 @@ class DQAgent(ReinforcementAgent):
             self.lastWindowAccumRewards = 0.0
         self.lastWindowAccumRewards += state.getScore()
 
+        if len(self.Reward_Tracker_100) > self.movingAverageWindowSize: 
+            self.Reward_Tracker_100.pop(0)
+        self.Reward_Tracker_100.append(state.getScore())
+
+        averageReward_100 = sum(self.Reward_Tracker_100) / len(self.Reward_Tracker_100)
+            # define the best model as the one with the highest average reward
+            # maybe we should kick it out of the if %100 statement as we would only take 
+            # models into consideration that are 100, 200, 300... but not inbetween
+        if averageReward_100 > self.best_reward:
+            self.best_reward = averageReward_100
+            self.best_model = self.double_Q.policy_network.state_dict()
+
+
         NUM_EPS_UPDATE = 100
         if self.episodesSoFar % NUM_EPS_UPDATE == 0:
             # print(f"Episode: {self.episodesSoFar}")
             windowAvg = self.lastWindowAccumRewards / float(NUM_EPS_UPDATE)
-
-            # define the best model as the one with the highest average reward
-            # maybe we should kick it out of the if %100 statement as we would only take 
-            # models into consideration that are 100, 200, 300... but not inbetween
-            if windowAvg > self.best_reward:
-                self.best_reward = windowAvg
-                self.best_model = self.double_Q.policy_network.state_dict()
-
 
             if self.episodesSoFar <= self.numTraining:
                 trainAvg = self.accumTrainRewards / float(self.episodesSoFar)
