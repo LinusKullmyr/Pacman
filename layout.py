632561e7c6691df458fd864baef3fe4d8b576c8b
kambei86 <157814893+kambei86@users.noreply.github.com>
2024-05-09 16:35:05 +0200

    Performance boost in creating of layout

--- layout.py
+++ layout.py
@@ -27,6 +27,7 @@ class Layout:
     """
 
     def __init__(self, layoutText):
+        # print(hash(tuple(layoutText)))
         self.width = len(layoutText[0])
         self.height = len(layoutText)
         self.walls = Grid(self.width, self.height, False)
@@ -46,11 +47,20 @@ class Layout:
         global VISIBILITY_MATRIX_CACHE
         if reduce(str.__add__, self.layoutText) not in VISIBILITY_MATRIX_CACHE:
             from game import Directions
+
             vecs = [(-0.5, 0), (0.5, 0), (0, -0.5), (0, 0.5)]
-            dirs = [Directions.NORTH, Directions.SOUTH,
-                    Directions.WEST, Directions.EAST]
-            vis = Grid(self.width, self.height, {Directions.NORTH: set(), Directions.SOUTH: set(
-            ), Directions.EAST: set(), Directions.WEST: set(), Directions.STOP: set()})
+            dirs = [Directions.NORTH, Directions.SOUTH, Directions.WEST, Directions.EAST]
+            vis = Grid(
+                self.width,
+                self.height,
+                {
+                    Directions.NORTH: set(),
+                    Directions.SOUTH: set(),
+                    Directions.EAST: set(),
+                    Directions.WEST: set(),
+                    Directions.STOP: set(),
+                },
+            )
             for x in range(self.width):
                 for y in range(self.height):
                     if self.walls[x][y] == False:
@@ -63,8 +73,7 @@ class Layout:
             self.visibility = vis
             VISIBILITY_MATRIX_CACHE[reduce(str.__add__, self.layoutText)] = vis
         else:
-            self.visibility = VISIBILITY_MATRIX_CACHE[reduce(
-                str.__add__, self.layoutText)]
+            self.visibility = VISIBILITY_MATRIX_CACHE[reduce(str.__add__, self.layoutText)]
 
     def isWall(self, pos):
         x, col = pos
@@ -79,13 +88,11 @@ class Layout:
         return (x, y)
 
     def getRandomCorner(self):
-        poses = [(1, 1), (1, self.height - 2), (self.width - 2, 1),
-                 (self.width - 2, self.height - 2)]
+        poses = [(1, 1), (1, self.height - 2), (self.width - 2, 1), (self.width - 2, self.height - 2)]
         return random.choice(poses)
 
     def getFurthestCorner(self, pacPos):
-        poses = [(1, 1), (1, self.height - 2), (self.width - 2, 1),
-                 (self.width - 2, self.height - 2)]
+        poses = [(1, 1), (1, self.height - 2), (self.width - 2, 1), (self.width - 2, self.height - 2)]
         dist, pos = max([(manhattanDistance(p, pacPos), p) for p in poses])
         return pos
 
@@ -97,7 +104,8 @@ class Layout:
         return "\n".join(self.layoutText)
 
     def deepCopy(self):
-        return Layout(self.layoutText[:])
+        return self
+        # return Layout(self.layoutText[:])
 
     def processLayoutText(self, layoutText):
         """
@@ -121,41 +129,41 @@ class Layout:
         self.agentPositions = [(i == 0, pos) for i, pos in self.agentPositions]
 
     def processLayoutChar(self, x, y, layoutChar):
-        if layoutChar == '%':
+        if layoutChar == "%":
             self.walls[x][y] = True
-        elif layoutChar == '.':
+        elif layoutChar == ".":
             self.food[x][y] = True
-        elif layoutChar == 'o':
+        elif layoutChar == "o":
             self.capsules.append((x, y))
-        elif layoutChar == 'P':
+        elif layoutChar == "P":
             self.agentPositions.append((0, (x, y)))
-        elif layoutChar in ['G']:
+        elif layoutChar in ["G"]:
             self.agentPositions.append((1, (x, y)))
             self.numGhosts += 1
-        elif layoutChar in ['1', '2', '3', '4']:
+        elif layoutChar in ["1", "2", "3", "4"]:
             self.agentPositions.append((int(layoutChar), (x, y)))
             self.numGhosts += 1
 
 
 def getLayout(name, back=2):
-    if name.endswith('.lay'):
-        layout = tryToLoad('layouts/' + name)
+    if name.endswith(".lay"):
+        layout = tryToLoad("layouts/" + name)
         if layout == None:
             layout = tryToLoad(name)
     else:
-        layout = tryToLoad('layouts/' + name + '.lay')
+        layout = tryToLoad("layouts/" + name + ".lay")
         if layout == None:
-            layout = tryToLoad(name + '.lay')
+            layout = tryToLoad(name + ".lay")
     if layout == None and back >= 0:
-        curdir = os.path.abspath('.')
-        os.chdir('..')
+        curdir = os.path.abspath(".")
+        os.chdir("..")
         layout = getLayout(name, back - 1)
         os.chdir(curdir)
     return layout
 
 
 def tryToLoad(fullname):
-    if(not os.path.exists(fullname)):
+    if not os.path.exists(fullname):
         return None
     f = open(fullname)
     try:
